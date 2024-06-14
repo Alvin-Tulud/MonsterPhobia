bd3ad094f39c06135eb383ae1d8666849fe9633d
Alvin-Tulud <alvintulud@gmail.com>
2024-06-13 21:19:50 -0700

    stuff

---
+++ MonsterPhobia/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Transform cam;
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = gameObject.GetComponent<Transform>();
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x > cam.position.x + 9)
+        {
+            //Debug.Log("right");
+            cam.position = new Vector3(cam.position.x + 18, cam.position.y, cam.position.z);
+        }
+        else if (player.position.x < cam.position.x - 9)
+        {
+            //Debug.Log("left");
+            cam.position = new Vector3(cam.position.x - 18, cam.position.y, cam.position.z);
+        }
+        else if (player.position.y > cam.position.y + 5)
+        {
+            //Debug.Log("up");
+            cam.position = new Vector3(cam.position.x, cam.position.y + 10, cam.position.z);
+        }
+        else if (player.position.y < cam.position.y - 5)
+        {
+            //Debug.Log("down");
+            cam.position = new Vector3(cam.position.x, cam.position.y - 10, cam.position.z);
+        }
+    }
+}
