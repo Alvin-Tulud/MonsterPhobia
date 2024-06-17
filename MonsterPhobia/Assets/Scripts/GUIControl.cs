32f4937e319e419865428fd1e781cf483a3f8b80
Alvin-Tulud <alvintulud@gmail.com>
2024-06-17 16:04:20 -0700

    turn book on off

--- MonsterPhobia/Assets/Scripts/GUIControl.cs
+++ MonsterPhobia/Assets/Scripts/GUIControl.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class GUIControl : MonoBehaviour
 {
-
+    GameObject book;
+    PlayerMove player;
+    FlashLightMove flashLight;
+    bool onoff;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        book = transform.GetChild(0).gameObject;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
+        flashLight = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetComponent<FlashLightMove>();
+
+        onoff = false;
     }
 
     private void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            book.SetActive(!onoff);
+
+
+            player.enabled = onoff;
+            flashLight.enabled = onoff;
+
+
+            onoff = !onoff;
+        }
     }
 }
