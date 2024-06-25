4eaef2d1edfea647444a84b6f876154d95e706a3
LePuncake321 <lucky713ph@yahoo.com>
2024-06-25 00:55:01 -0700

    Start of a new script for Monster
    Will cover the changes in behaviour from Aggro to Passive, from the targets to the speed

---
+++ MonsterPhobia/Assets/Scripts/States/MonsterBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Randomly generate a monsterStruct
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do behavior stuff with MonsterPassive
+            // Change targets between corners for patrols, or stalking
+            // Change speed for certain kinds of behaviour
+        // Do chasing stuff with MonsterAggro
+            // Change target to Player character
+    }
+}
