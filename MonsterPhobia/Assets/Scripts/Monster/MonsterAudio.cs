55fd88b3a210d2d8c25743878fb9691349a7416f
mm8wood <tw802326@gmail.com>
2024-06-29 00:06:44 -0700

    sound push
    - new script for monster audio
    - put audio listener on main guy
    - added 5 footstep SFX variations
    - plays footstep sounds which get louder as villain approaches

---
+++ MonsterPhobia/Assets/Scripts/Monster/MonsterAudio.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAudio : MonoBehaviour
+{
+    AudioSource footstepSource;
+    AudioSource sfxSource;
+    public float fsDelay = .5f;
+
+    public List<AudioClip> footsteps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        footstepSource = GetComponents<AudioSource>()[0];
+        sfxSource = GetComponents<AudioSource>()[1];
+        StartCoroutine(PlayFootsteps(fsDelay));
+
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    int RandomFootstepSFX()
+    {
+        int index = Random.Range(0, footsteps.Count);
+        return index;
+    }
+
+    IEnumerator PlayFootsteps (float delay)
+    {
+        while (true)
+        {
+            Debug.Log("FOOTSTEP");
+            AudioClip clip = footstepSource.clip = footsteps[RandomFootstepSFX()];
+            footstepSource.clip = clip;
+            footstepSource.Play();
+            yield return new WaitForSeconds(delay);
+        }
+
+
+    }
+}
